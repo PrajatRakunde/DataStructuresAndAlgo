aa2e8ff2eb3dc8b4fa06d7a41bf3ba061a2da856
prajat <prajatrku@gmail.com>
2020-11-02 10:35:44 +0530

    Added better implementation isPermutation2

--- IfTwoStringsPermutation.java
+++ IfTwoStringsPermutation.java
@@ -4,10 +4,10 @@ public class PermutationOfTwoStrings {
 
 	public static void main(String[] args) {
 		
-		String x = "abcd";
-		String y = "dbca";
+		String x = "aaabcdb";
+		String y = "abcdqwe";
 		
-		System.out.println(isPermtation(x,y));
+		System.out.println(isPermtation2(x,y));
 		
 	}
 
@@ -29,6 +29,38 @@ public class PermutationOfTwoStrings {
 				
 			}
 			
+		}
+		
+		return false;
+	}
+	
+	static boolean isPermtation2(String x, String y) {
+		
+		if(x.length() == y.length()) {
+			char[] x1 = x.toCharArray();
+			char[] y1 = y.toCharArray();
+			int[] letters = new int[128];
+			
+			for(char c : x1) {
+				letters[c]++;
+			}
+			
+			for(int i = 0; i<letters.length; i++) {
+				System.out.print(letters[i]);
+			}
+			
+			for(int i = 0; i<y.length(); i++) {
+				
+				int c = y.charAt(i);
+				letters[c]--;
+				System.out.println(letters[c]);
+				System.out.println(c);
+				if(letters[c] < 0) 
+					return false;
+				
+				
+			}
+			return true;
 		}
 		
 		return false;
